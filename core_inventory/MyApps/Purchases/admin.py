dcc82adb50fcb999e75b5b626b784ba81fecf1e7
JuanC-27 <juang20133@gmail.com>
2023-02-06 21:16:01 -0500

    Fix responsive navbar and optimize svg icons

--- core_inventory/MyApps/Purchases/admin.py
+++ core_inventory/MyApps/Purchases/admin.py
@@ -6,18 +6,17 @@ class PurchaseDetailInline(admin.TabularInline):
     model = PurchaseDetail
     extra = 1
     exclude = ['total',]
-
+    
 class PurchaseAdmin(admin.ModelAdmin):
     inlines = [PurchaseDetailInline,]
     filter_horizontal = ['product',]
     list_display = ('purchase_id','created_at', 'total', 'pdf')
     exclude = ['purchase_id','total']
     
+    def has_change_permission(self, request, obj=None):
+        return False
+    
     def pdf(self,queryset):
         return format_html('<a class="text-decoration-none" href="/compras/generatePDF/{}">📂<a/>', queryset)
-        
-    
-
-
 
 admin.site.register(Purchase, PurchaseAdmin)
