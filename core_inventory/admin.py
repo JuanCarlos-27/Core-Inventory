bcefea8a662c9fd54a075b5ac3be3ba9f93ab655
JuanC-27 <juang20133@gmail.com>
2023-03-05 10:58:21 -0500

    Add charts section view only for superusers

--- core_inventory/admin.py
+++ core_inventory/admin.py
@@ -2,31 +2,34 @@ from django.contrib import admin
 from django.contrib.admin.views.decorators import staff_member_required
 from django.shortcuts import render
 from django.urls import path
+from django.contrib.auth.decorators import user_passes_test
 
 
-@staff_member_required
+@user_passes_test(lambda u: u.is_superuser)
 def admin_statistics_view(request):
     return render(request, 'admin/charts.html', {
         'title': 'Graficos'
     })
-
+    
 class CustomAdminSite(admin.AdminSite):
     def get_app_list(self, request):
         app_list = super().get_app_list(request)
-        app_list += [
-            {
-                'name': 'Reportes',
-                'app_label': 'Gráficos',
-                'models': [
-                    {
-                        'name': 'Gráficos',
-                        'object_name': 'Gráficos',
-                        'admin_url': '/admin/statistics/',
-                        'view_only': False,
-                    }
-                ],
-            },
-        ]
+        
+        if(request.user.is_superuser):
+            app_list += [
+                {
+                    'name': 'Reportes',
+                    'app_label': 'Gráficos',
+                    'models': [
+                        {
+                            'name': 'Gráficos',
+                            'object_name': 'Gráficos',
+                            'admin_url': '/admin/statistics/',
+                            'view_only': False,
+                        }
+                    ],
+                },
+            ]
         return app_list
 
     def get_urls(self):
