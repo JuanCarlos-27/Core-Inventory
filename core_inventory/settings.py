d446a563be7162c51cf192dbdcf3dddc32506e12
JuanC-27 <juang20133@gmail.com>
2023-03-12 19:28:40 -0500

    Uncomment static url

--- core_inventory/settings.py
+++ core_inventory/settings.py
@@ -121,14 +121,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-# STATIC_URL = 'static/'
+STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'static'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Development
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR,'static'),
-)
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR,'static'),
+# )
 
 # Default primary key field type
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
